a15d9679313da1282207e0d10ab5cf4325dfc899
Michael Bernasol <mfbernasol@gmail>
2019-10-26 22:14:19 -0500

    Initial Commit

---
+++ Foody/ViewController.swift
@@ -0,0 +1,78 @@
+//
+//  ViewController.swift
+//  Foody
+//
+//  Created by Michael Bernasol on 10/26/19.
+//  Copyright © 2019 Michael Bernasol. All rights reserved.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+        
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = userPickedImage
+            
+            guard let ciimage = CIImage(image: userPickedImage) else{
+                fatalError("Could not convert to CIImage")
+            }
+            
+            detect(image: ciimage)
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    func detect(image: CIImage){
+        
+        guard let model = try? VNCoreMLModel(for:Inceptionv3().model) else{
+            fatalError("Loading CoreML Model Failed")
+        }
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else{
+                fatalError("Model failed to process")
+            }
+            if let firstResult = results.first{
+                if firstResult.identifier.contains("water"){
+                    self.navigationItem.title = "That is water! Quench yourself!"
+                }else{
+                    self.navigationItem.title = "That isn't water. Don't drink it"
+                }
+            }
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do{
+            try handler.perform([request])
+        }catch{
+            print(error)
+        }
+        
+    }
+
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+}
+
